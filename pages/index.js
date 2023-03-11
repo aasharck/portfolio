3cab30fb25e33ba8dfdd4935dad6d5d10d4ff792
Aashar ck <aasharck@gmail.com>
2023-03-12 00:15:33 +0530

    added some projects

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,8 @@ export default function Home() {
             <div className='text-white font-bold text-lg lg:text-2xl'>Audit Findings</div>
             <div className='flex mt-4'>
               <div className='w-1/3 font-bold text-slate-600'><span className='text-2xl lg:text-5xl text-white'>3</span> High</div>
-              <div className='w-1/3 font-bold text-slate-600'><span className='text-2xl lg:text-5xl text-white'>4</span> Med</div>
-              <div className='w-1/3 font-bold text-slate-600'><span className='text-2xl lg:text-5xl text-white'>10</span> Low/Gas</div>
+              <div className='w-1/3 font-bold text-slate-600'><span className='text-2xl lg:text-5xl text-white'>2</span> Med</div>
+              <div className='w-1/3 font-bold text-slate-600'><span className='text-2xl lg:text-5xl text-white'>2</span> Low/QA</div>
             </div>
           </div>
           <div className='block ml-3 bg-white rounded-xl w-1/3 p-5 pt-9'>
@@ -44,21 +44,31 @@ export default function Home() {
             <div className='text-black text-3xl lg:text-6xl font-bold mt-3'>Cal.com</div>
           </div>
         </div>
-        <div className='flex bg-black p-3 rounded-xl mt-3'>
-          <div className='block p-5 w-2/3'>
+        <div className='md:flex block bg-black p-3 rounded-xl mt-3'>
+          <div className='block md:p-5 p-2 w-2/3 md:py-20 py-8'>
             <h1 className='pl-3 text-5xl font-bold text-white'>keepshipping</h1>
-            <p className='pl-3 mt-2 text-white font-bold lg:text-sm text-xs'>I code, audit smart contracts and occassionally build stuffs that no one cares.</p>
+            <p className='pl-3 mt-2 text-white lg:text-sm text-xs'>a platform that lets people share what they shipped with the world. Product hunt but with less bots and less users(duh!).</p>
           </div>
-          <div className='w-1/3'>
-            <Image src="/readingmood.png" width={120} height={120}/>
+          <div className='block mx-auto w-1/3 py-5'>
+            <Image src="/readingmood.jpg" width={300} height={300}/>
           </div>
         </div>
-        <div className='flex bg-black p-3 rounded-xl mt-3'>
-          <h1 className='pl-3 text-2xl font-bold text-white'>copygen</h1>
-        </div>
-        <div className='flex bg-black p-3 rounded-xl mt-3'>
-          <h1 className='pl-3 text-2xl font-bold text-white'>reading mood</h1>
-        </div>
+        <div className='md:flex block bg-black p-3 rounded-xl mt-3'>
+        <div className='block md:p-5 p-2 w-2/3 md:py-20 py-8'>
+            <h1 className='pl-3 text-5xl font-bold text-white'>copygen</h1>
+            <p className='pl-3 mt-2 text-white lg:text-sm text-xs'>a useful copywriting tool for dropshippers and online sellers that lets them create product copies and ad copies just from a product link (like alibaba)</p>
+          </div>
+          <div className='block mx-auto w-1/3 py-5'>
+            <Image src="/readingmood.jpg" width={300} height={300}/>
+          </div>        </div>
+        <div className='md:flex block bg-black p-3 rounded-xl mt-3'>
+        <div className='block md:p-5 p-2 w-2/3 md:py-20 py-8'>
+            <h1 className='pl-3 text-5xl font-bold text-white'>readingmood</h1>
+            <p className='pl-3 mt-2 text-white lg:text-sm text-xs'>Find the perfect song that exactly matches the book that you are reading. Build this as a part of a challenge from Buildspace</p>
+          </div>
+          <div className='block mx-auto w-1/3 py-5'>
+            <Image src="/readingmood.jpg" width={300} height={300}/>
+          </div>        </div>
       </main>
     </>
   );
